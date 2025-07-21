a5a9bcb0af2e168aa58d6e445a884a7851df1530
Cesar Delgado <beettlle@gmail.com>
2025-07-21 10:48:14 -0700

    Bump version to 1.4.2 in service worker and version configuration files

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // Version configuration for RobinsonLimitMP
-const APP_VERSION = '1.4.1';
+const APP_VERSION = '1.4.2';
 
 // Export for use in other modules
 if (typeof module !== 'undefined' && module.exports) {
